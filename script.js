5e3a38b8b0faaf0ed9de23ceefaacf5980956c01
Henry North <henrynorth@outlook.com>
2019-09-10 17:26:40 +0100

    added timer

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ let mazeHeight;
 let mazeWidth;
 let playerIcon;
 
-class Player {
+class PlayerIcon {
   constructor() {
-    this.col = 0;
+    this.column = 0;
     this.row = 0;
   }
 }
 
 class MazeCell {
-  constructor(col, row) {
-    this.col = col;
+  constructor(column, row) {
+    this.column = column;
     this.row = row;
 
     this.rightWall = true;
@@ -26,45 +26,52 @@ class MazeCell {
 }
 
 class Maze {
-  constructor(cols, rows, cellSize) {
+  constructor(columns, rows, cellSize) {
     this.backgroundColor = "#ffffff";
     this.endColor = "#00ff00";
     this.mazeColor = "#000000";
     this.playerColor = "#ff0000";
     this.rows = rows;
-    this.cols = cols;
+    this.columns = columns;
     this.cellSize = cellSize;
 
     this.cells = [];
 
-    this.generate()
+    this.isGameStarted = false;
+
+    this.totalSeconds = 0;
+
+    this.generateMaze()
   }
 
-  generate() {
+  generateMaze() {
     mazeHeight = this.rows * this.cellSize;
-    mazeWidth = this.cols * this.cellSize;
+    mazeWidth = this.columns * this.cellSize;
 
     canvas.height = mazeHeight;
     canvas.width = mazeWidth;
     canvas.style.height = mazeHeight;
     canvas.style.width = mazeWidth;
 
-    for (let col = 0; col < this.cols; col++) {
-      this.cells[col] = [];
+    playerIcon.column = 0;
+    playerIcon.row = 0;
+
+    for (let column = 0; column < this.columns; column++) {
+      this.cells[column] = [];
       for (let row = 0; row < this.rows; row++) {
-        this.cells[col][row] = new MazeCell(col, row);
+        this.cells[column][row] = new MazeCell(column, row);
       }
     }
 
-    let randomCol = Math.floor(Math.random() * this.cols);
+    let randomColumn = Math.floor(Math.random() * this.columns);
     let randomRow = Math.floor(Math.random() * this.rows);
 
     let stack = [];
-    stack.push(this.cells[randomCol][randomRow]);
+    stack.push(this.cells[randomColumn][randomRow]);
 
     let currentCell;
     let nextCell;
-    let dir;
+    let directory;
     let foundNeighbor;
 
     while (this.hasUntouched(this.cells)) {
@@ -74,36 +81,36 @@ class Maze {
         nextCell = null;
         foundNeighbor = false;
         do {
-          dir = Math.floor(Math.random() * 4);
-          switch (dir) {
+          directory = Math.floor(Math.random() * 4);
+          switch (directory) {
             case 0:
-              if (currentCell.col !== (this.cols - 1) && !this.cells[currentCell.col + 1][currentCell.row].visited) {
+              if (currentCell.column !== (this.columns - 1) && !this.cells[currentCell.column + 1][currentCell.row].visited) {
                 currentCell.rightWall = false;
-                nextCell = this.cells[currentCell.col + 1][currentCell.row];
+                nextCell = this.cells[currentCell.column + 1][currentCell.row];
                 nextCell.leftWall = false;
                 foundNeighbor = true;
               }
               break;
             case 1:
-              if (currentCell.row !== 0 && !this.cells[currentCell.col][currentCell.row - 1].visited) {
+              if (currentCell.row !== 0 && !this.cells[currentCell.column][currentCell.row - 1].visited) {
                 currentCell.topWall = false;
-                nextCell = this.cells[currentCell.col][currentCell.row - 1];
+                nextCell = this.cells[currentCell.column][currentCell.row - 1];
                 nextCell.bottomWall = false;
                 foundNeighbor = true;
               }
               break;
             case 2:
-              if (currentCell.row !== (this.rows - 1) && !this.cells[currentCell.col][currentCell.row + 1].visited) {
+              if (currentCell.row !== (this.rows - 1) && !this.cells[currentCell.column][currentCell.row + 1].visited) {
                 currentCell.bottomWall = false;
-                nextCell = this.cells[currentCell.col][currentCell.row + 1];
+                nextCell = this.cells[currentCell.column][currentCell.row + 1];
                 nextCell.topWall = false;
                 foundNeighbor = true;
               }
               break;
             case 3:
-              if (currentCell.col !== 0 && !this.cells[currentCell.col - 1][currentCell.row].visited) {
+              if (currentCell.column !== 0 && !this.cells[currentCell.column - 1][currentCell.row].visited) {
                 currentCell.leftWall = false;
-                nextCell = this.cells[currentCell.col - 1][currentCell.row];
+                nextCell = this.cells[currentCell.column - 1][currentCell.row];
                 nextCell.rightWall = false;
                 foundNeighbor = true;
               }
@@ -121,9 +128,9 @@ class Maze {
   }
 
   hasUntouched() {
-    for (let col = 0; col < this.cols; col++) {
+    for (let column = 0; column < this.columns; column++) {
       for (let row = 0; row < this.rows; row++) {
-        if (!this.cells[col][row].visited) {
+        if (!this.cells[column][row].visited) {
           return true;
         }
       }
@@ -133,10 +140,10 @@ class Maze {
 
   hasUntouchedNeighbor(mazeCell) {
     return (
-        (mazeCell.col !== 0               && !this.cells[mazeCell.col - 1][mazeCell.row].visited) ||
-        (mazeCell.col !== (this.cols - 1) && !this.cells[mazeCell.col + 1][mazeCell.row].visited) ||
-        (mazeCell.row !== 0               && !this.cells[mazeCell.col][mazeCell.row - 1].visited) ||
-        (mazeCell.row !== (this.rows - 1) && !this.cells[mazeCell.col][mazeCell.row + 1].visited)
+        (mazeCell.column !== 0 && !this.cells[mazeCell.column - 1][mazeCell.row].visited) ||
+        (mazeCell.column !== (this.columns - 1) && !this.cells[mazeCell.column + 1][mazeCell.row].visited) ||
+        (mazeCell.row !== 0 && !this.cells[mazeCell.column][mazeCell.row - 1].visited) ||
+        (mazeCell.row !== (this.rows - 1) && !this.cells[mazeCell.column][mazeCell.row + 1].visited)
       );
   }
 
@@ -145,85 +152,130 @@ class Maze {
     context.fillRect(0, 0, mazeHeight, mazeWidth);
 
     context.fillStyle = this.endColor;
-    context.fillRect((this.cols - 1) * this.cellSize, (this.rows - 1) * this.cellSize, this.cellSize, this.cellSize);
+    context.fillRect((this.columns - 1) * this.cellSize, (this.rows - 1) * this.cellSize, this.cellSize, this.cellSize);
 
     context.strokeStyle = this.mazeColor;
     context.strokeRect(0, 0, mazeHeight, mazeWidth);
 
-    for (let col = 0; col < this.cols; col++) {
+    for (let column = 0; column < this.columns; column++) {
       for (let row = 0; row < this.rows; row++) {
-        if (this.cells[col][row].rightWall) {
+        if (this.cells[column][row].rightWall) {
           context.beginPath();
-          context.moveTo((col + 1) * this.cellSize, row * this.cellSize);
-          context.lineTo((col + 1) * this.cellSize, (row + 1) * this.cellSize);
+          context.moveTo((column + 1) * this.cellSize, row * this.cellSize);
+          context.lineTo((column + 1) * this.cellSize, (row + 1) * this.cellSize);
           context.stroke();
         }
-        if (this.cells[col][row].topWall) {
+        if (this.cells[column][row].topWall) {
           context.beginPath();
-          context.moveTo(col * this.cellSize, row * this.cellSize);
-          context.lineTo((col + 1) * this.cellSize, row * this.cellSize);
+          context.moveTo(column * this.cellSize, row * this.cellSize);
+          context.lineTo((column + 1) * this.cellSize, row * this.cellSize);
           context.stroke();
         }
-        if (this.cells[col][row].bottomWall) {
+        if (this.cells[column][row].bottomWall) {
           context.beginPath();
-          context.moveTo(col * this.cellSize, (row + 1) * this.cellSize);
-          context.lineTo((col + 1) * this.cellSize, (row + 1) * this.cellSize);
+          context.moveTo(column * this.cellSize, (row + 1) * this.cellSize);
+          context.lineTo((column + 1) * this.cellSize, (row + 1) * this.cellSize);
           context.stroke();
         }
-        if (this.cells[col][row].leftWall) {
+        if (this.cells[column][row].leftWall) {
           context.beginPath();
-          context.moveTo(col * this.cellSize, row * this.cellSize);
-          context.lineTo(col * this.cellSize, (row + 1) * this.cellSize);
+          context.moveTo(column * this.cellSize, row * this.cellSize);
+          context.lineTo(column * this.cellSize, (row + 1) * this.cellSize);
           context.stroke();
         }
       }
     }
     context.fillStyle = this.playerColor;
-    context.fillRect((player.col * this.cellSize) + 2, (player.row * this.cellSize) + 2, this.cellSize - 4, this.cellSize - 4);
+    context.fillRect((playerIcon.column * this.cellSize) + 2, (playerIcon.row * this.cellSize) + 2, this.cellSize - 4, this.cellSize - 4);
   }
 }
 
-onGenerate = () => {
-  maze.cols = document.getElementById("cols").value;
-  maze.rows = document.getElementById("rows").value;
-  maze.generate();
+onGenerateMazeClick = () => {
+  mazeSize = document.getElementById("mazeSize").value;
+
+  minMaxSizeWarning = document.getElementById("minMaxSizeWarning");
+
+  if (mazeSize > 30) {
+    minMaxSizeWarning.innerHTML = "Maximium maze size is 30";
+  }
+  else if (mazeSize < 5) {
+    minMaxSizeWarning.innerHTML = "Minimium maze size is 5";
+  }
+  else {
+    maze.columns = mazeSize;
+    maze.rows = mazeSize;
+    minMaxSizeWarning.innerHTML = "";
+    maze.generateMaze();
+  }
+}
+
+onStartButtonClick = () => {
+  maze.isGameStarted = true;
+
+  startTimer = setInterval(startTimerCount, 1000);
+  document.getElementById("startButton").disabled = true;
+  document.getElementById("stopButton").disabled = false;
+}
+
+onStopButtonClick = () => {
+  maze.isGameStarted = false;
+  maze.totalSeconds = 0;
+
+  clearInterval(startTimer);
+  document.getElementById("timer").innerHTML = maze.totalSeconds + " Seconds";
+  document.getElementById("startButton").disabled = false;
+  document.getElementById("stopButton").disabled = true;
+}
+
+startTimerCount = (totalSeconds) => {
+  maze.totalSeconds++;
+
+  document.getElementById("timer").innerHTML = maze.totalSeconds + " Seconds";
 }
 
 onKeyDown = (event) => {
-  switch (event.keyCode) {
-    case 37:
-      if (!maze.cells[player.col][player.row].leftWall) {
-        player.col = player.col - 1;
-      }
-      break;
-    case 39:
-      if (!maze.cells[player.col][player.row].rightWall) {
-        player.col = player.col + 1;
-      }
-      break;
-    case 40:
-      if (!maze.cells[player.col][player.row].bottomWall) {
-        player.row = player.row + 1;
-      }
-      break;
-    case 38:
-      if (!maze.cells[player.col][player.row].topWall) {
-        player.row = player.row - 1;
-      }
-      break;
-    default:
-      break;
+  if (maze.isGameStarted === true) {
+    switch (event.keyCode) {
+      case 37:
+        if (!maze.cells[playerIcon.column][playerIcon.row].leftWall) {
+          playerIcon.column = playerIcon.column - 1;
+        }
+        break;
+      case 39:
+        if (!maze.cells[playerIcon.column][playerIcon.row].rightWall) {
+          playerIcon.column = playerIcon.column + 1;
+        }
+        break;
+      case 38:
+        if (!maze.cells[playerIcon.column][playerIcon.row].topWall) {
+          playerIcon.row = playerIcon.row - 1;
+        }
+        break;
+      case 40:
+        if (!maze.cells[playerIcon.column][playerIcon.row].bottomWall) {
+          playerIcon.row = playerIcon.row + 1;
+        }
+        break;
+      default:
+        break;
+    }
+    maze.redraw();
   }
-  maze.redraw();
+  else if (maze.isGameStarted === false) {
+    return false;
+  }
 }
 
 onLoad = () => {
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
 
-  player = new Player();
+  playerIcon = new PlayerIcon();
   maze = new Maze(20, 20, 25);
 
   document.addEventListener("keydown", onKeyDown);
-  document.getElementById("generate").addEventListener("click", onGenerate);
+  document.getElementById("generate").addEventListener("click", onGenerateMazeClick);
+  document.getElementById("startButton").addEventListener("click", onStartButtonClick);
+  document.getElementById("stopButton").addEventListener("click", onStopButtonClick);
+  document.getElementById("stopButton").disabled = true;
 }
